bd931b15cd7c662503bfe42736c418cefeb12f02
daviidy <yaodavidarmel@gmail.com>
2024-09-29 23:28:29 -0500

    updated education and projects sections

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.tothenew.com',
+        port: '',
+        pathname: '/blog/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.gcore.pro',
+        port: '',
+        pathname: '/**'
+      }
+    ],
+  },
 };
 
 export default nextConfig;
